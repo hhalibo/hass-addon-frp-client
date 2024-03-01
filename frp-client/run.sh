e5fe64eea6464fac629a8ebb09f10977b8d3834c
hhalibo <gao13903408669@gmail.com>
2024-03-01 15:35:13 +0800

    Update run.sh

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -10,12 +10,10 @@ function stop_frpc() {
 
 bashio::log.info "Copying configuration."
 cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
-sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
-sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
-sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
-sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
-sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
-sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
+sed -i "s/server_addr = \"your_server_addr\"/server_addr = \"$(bashio::config 'server_addr')\"/" $CONFIG_PATH
+sed -i "s/server_port = 17000/server_port = $(bashio::config 'server_port')/" $CONFIG_PATH
+sed -i "s/user = \"admin\"/user = \"$(bashio::config 'user')\"/" $CONFIG_PATH
+sed -i "s/meta_token = \"123456789\"/meta_token = \"$(bashio::config 'meta_token')\"/" $CONFIG_PATH
 sed -i "s/customDomains = \[\"your_domain\"\]/customDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
 sed -i "s/name = \"your_proxy_name\"/name = \"$(bashio::config 'proxyName')\"/" $CONFIG_PATH
 
